944ae5bbdf318b9dbe87225af00f3d2b3205fcf7
Devi Raju <deviraju999@gmail.com>
2024-06-27 22:56:46 +0530

    add search

--- movies/views.py
+++ movies/views.py
@@ -14,6 +14,11 @@ class MovieList(ListView):
 def movie_list(request):
     movie_list = MoviesData.objects.all()
 
+    movie_search = request.GET.get('movie_search')
+
+    if movie_search != '' and movie_search is not None:
+        movie_list = movie_list.filter(name__icontains = movie_search)
+
     pagination = Paginator(movie_list, 3)
     page = request.GET.get('page')
     movie_list = pagination.get_page(page)
